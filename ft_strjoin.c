d3e9ab3213b8ef6cc86e5d838c2e98166dc6346a
Konstantin Belov <kbelov@e1z2r5p33.42.us.org>
2019-02-28 21:03:13 -0800

    v 1.0

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbelov <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/18 11:47:50 by kbelov            #+#    #+#             */
+/*   Updated: 2019/02/20 19:41:03 by kbelov           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <stdlib.h>
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	char	*s3;
+
+	if (!s1 && !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	s3 = (char *)malloc(sizeof(char) * len);
+	if (!s3)
+		return (NULL);
+	ft_strcpy(s3, s1);
+	ft_strcat(s3, s2);
+	return (s3);
+}
